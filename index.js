b91a3688941bedec218c1f0e8cd7d6255d4c0e72
Alexander kagerer <kagerer19@gmail.com>
2020-05-07 16:31:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function add2(n) {
   return total;
 }
 
-var funkyFunction = funkyFunction; function(funkyFunction) {
+var funkyFunction = funkyFunction; function() {
   return function() {
     return "FUNKY!";
   }
